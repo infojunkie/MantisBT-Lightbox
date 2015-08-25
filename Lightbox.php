9cf6fdbb6514e7c18692b45a96313bb249a60e04
Kaue Santoja <shinjiiraki@gmail.com>
2015-08-25 01:20:53 -0300

    Version 0.2
    Version 0.2 released!
    Added config for the plugin: Now user can choose where he wants a
    lightbox
    New english and portuguese strings
    Minor changes on README.md

--- Lightbox.php
+++ Lightbox.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Lightbox Integration
  * Copyright (C) 2015 Karim Ratib (karim.ratib@gmail.com) and Kaue Santoja (shinjiiraki@gmail.com)
@@ -17,22 +18,33 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  * or see http://www.gnu.org/licenses/.
  */
-
 class LightboxPlugin extends MantisPlugin {
+
     function register() {
-        $this->name = plugin_lang_get( 'title' );
-        $this->description = plugin_lang_get( 'description' );
-        $this->version = '0.1.1';
+        $this->name = plugin_lang_get('title');
+        $this->description = plugin_lang_get('description');
+        $this->version = '0.2';
+        $this->page = 'config';
         $this->requires = array(
-            'MantisCore' => '1.2',
+            'MantisCore' => '>=1.2, <1.4',
         );
         // don't need jQuery in Mantis >= 1.3.0
-        if( version_compare( MANTIS_VERSION, '1.3', '<' ) === true ) {
+        if (version_compare(MANTIS_VERSION, '1.3', '<') === true) {
             $this->requires['jQuery'] = '>= 1.11.0';
         }
         $this->author = 'Karim Ratib and Kaue Santoja';
         $this->contact = 'kaue@santoja.com.br';
         $this->url = 'https://github.com/santoja/Lightbox';
+    }
+
+    /**
+     * Default plugin configuration.
+     */
+    function config() {
+        return array(
+            'display_on_img_preview' => ON,
+            'display_on_img_link' => OFF,
+        );
     }
 
     function hooks() {
@@ -42,11 +54,16 @@ class LightboxPlugin extends MantisPlugin {
     }
 
     function add_lightbox($event) {
-	$currentUrl = explode('/',$_SERVER['PHP_SELF']);
+        $currentUrl = explode('/', $_SERVER['PHP_SELF']);
         if (end($currentUrl) !== 'view.php') return;
-		return '<link href="'.plugin_file('lightbox/css/lightbox.css').'" rel="stylesheet">
-			<script type="text/javascript"> var lightboxlocation = "'.plugin_file('lightbox/js/lightbox-min.js').'";</script>
-			<script type="text/javascript" src="'.plugin_file('Lightbox.js').'"></script>';
-
+        
+        return '        <script type="text/javascript">'.
+                        'var lightbox_display_on_img_preview = '.plugin_config_get('display_on_img_preview').';'.
+                        'var lightbox_display_on_img_link = '.plugin_config_get('display_on_img_link').';'.
+                        ' var lightboxlocation = "' . plugin_file('lightbox/js/lightbox-min.js') . '";'.
+                        '</script>'.
+                        '<link href="' . plugin_file('lightbox/css/lightbox.css') . '" rel="stylesheet">'.
+			'<script type="text/javascript" src="' . plugin_file('Lightbox.js') . '"></script>';
     }
+
 }
--- files/Lightbox.js
+++ files/Lightbox.js
@@ -1,6 +1,11 @@
 (function ($) {
-        $(document).ready(function () {
+    $(document).ready(function () {
+        if(lightbox_display_on_img_preview){
             $('#attachments').parent().parent().find('a').find('img').parent().attr('rel', 'lightbox');
-			$.getScript(lightboxlocation);
-        });
-    })(jQuery);
+        }
+        if(lightbox_display_on_img_link){
+            $('#attachments').parent().parent().find('.small').prev().prev().prev().attr('rel', 'lightbox');
+        }
+        $.getScript(lightboxlocation);
+    });
+})(jQuery);
